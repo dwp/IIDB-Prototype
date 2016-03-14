5d797af3be24e490185604148fbff5f572870f90
Paul Smith <mail@airfront.co.uk>
2016-03-14 10:44:19 +0000

    adds step4 and updates so that postData is always accessible without creating routes each time

--- server.js
+++ server.js
@@ -69,8 +69,8 @@ app.use(function (req, res, next) {
 // Add variables that are available in all views
 app.use(function (req, res, next) {
   _.merge(res.locals,{
-    serviceName: config.cookieText,
-    cookieText: config.serviceName,
+    serviceName: config.serviceName,
+    cookieText: config.cookieText,
     path: req.params[0],
     postData: (req.body ? req.body : false)
   });
