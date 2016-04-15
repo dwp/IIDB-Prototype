132505c976792270ab9a1541c4408d8b3bc797b9
Darren Silcox <Silky2012@Silkster-MacPro.local>
2016-04-15 09:27:44 +0100

    Starts Alpha 08

---
+++ app/views/alpha/alpha-08/version_routes.js
@@ -0,0 +1,46 @@
+module.exports = function(router, config) {
+  router.all(config.protoPaths.step, function(req,res,next){
+
+    var requestedPage = req.params.step,
+        postData = req.body || {};
+
+    switch(requestedPage) {
+
+      case 'location':
+        if(postData['reason'] == 'other') {
+          res.redirect('ineligible');
+        }
+      break;
+
+      case 'working':
+        if(postData['withinregion'] == 'false') {
+          res.redirect('ineligible');
+        }
+      break;
+
+      case 'self_employed':
+        if(postData['atwork'] == 'false') {
+          res.redirect('ineligible');
+        }
+      break;
+
+      case 'self_employed_class1':
+        if(postData['selfemployed'] == 'false') {
+          res.redirect('employer_details');
+        }
+      break;
+
+      case 'employer_details':
+        if(postData['payclass1'] == 'false') {
+          res.redirect('ineligible');
+        }
+      break;
+
+    }
+
+    next();
+
+  });
+
+  return router;
+}
