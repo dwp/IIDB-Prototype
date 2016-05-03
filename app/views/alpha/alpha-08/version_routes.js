920a4a3afa3d49dc3a3e09bf04a628f80e2d5f60
Darren Silcox <Silky2012@Silkster-MacPro.local>
2016-05-03 13:25:39 +0100

    Update to routes JS

--- app/views/alpha/alpha-08/version_routes.js
+++ app/views/alpha/alpha-08/version_routes.js
@@ -18,14 +18,37 @@ module.exports = function(router, config) {
         }
       break;
 
-     // Self employed checker
+       // Self employed main page          
       case 'self_checker':
+        if(postData['selfemployed'] == 'true') {
+          res.redirect('self_employed_class1');
+        }
+        else if (postData['selfemployed'] == 'false') {
+          res.redirect('employer_details');
+        } 
+        else if (postData['selfemployed'] == 'unsure') {
+          res.redirect('self_checker');
+        } 
+      break;
+            
+    // Self checker page          
+      case 'self_employed_class1':
         if(postData['selfchecker'] == 'true') {
           res.redirect('self_employed_class1');
         }
         else if (postData['selfchecker'] == 'false') {
           res.redirect('employer_details');
+        } 
+      break;
+            
+    // Self class 1 page          
+      case 'employer_details':
+        if(postData['payclass1"'] == 'true') {
+          res.redirect('employer_details');
         }
+        else if (postData['payclass1'] == 'false') {
+          res.redirect('ineligible');
+        } 
       break;
 
 
