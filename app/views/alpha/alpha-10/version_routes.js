fa7a46fd9d202d7df5d5008d63b38bb79e8730ac
Darren Silcox <dsilcox.digitaldwp@gmail.com>
2016-05-10 14:33:20 +0100

    Changes for trello linking and putting alpha banner on all page whilst some titles and some spelling

---
+++ app/views/alpha/alpha-10/version_routes.js
@@ -0,0 +1,71 @@
+module.exports = function(router, config) {
+  router.all(config.route, function(req,res,next){
+
+    var requestedPage = req.params.step,
+        postData = req.body || {};
+
+    switch(requestedPage) {
+
+      case 'location':
+        if(postData['reason'] == 'other') {
+          res.redirect('ineligible');
+        }
+      break;
+
+      case 'working':
+        if(postData['withinregion'] == 'false') {
+          res.redirect('ineligible');
+        }
+      break;
+
+       // Self employed main page          
+      case 'elligible_consent':
+        if(postData['selfemployed'] == 'true') {
+          res.redirect('self_employed_class1');
+        }
+        else if (postData['selfemployed'] == 'false') {
+          res.redirect('elligible_consent');
+        } 
+        else if (postData['selfemployed'] == 'unsure') {
+          res.redirect('self_checker');
+        } 
+        // Self checker page 
+        if(postData['selfchecker'] == 'true') {
+          res.redirect('self_employed_class1');
+        }
+        else if (postData['selfchecker'] == 'false') {
+          res.redirect('elligible_consent');
+        } 
+        // Self class 1 page //
+        if(postData['payclass1"'] == 'true') {
+          res.redirect('elligible_consent');
+        }
+        else if (postData['payclass1'] == 'false') {
+          res.redirect('ineligible');
+        } 
+      break;
+            
+        // Medical Professionals //
+        case 'medical_names':
+        if(postData['seek_medical'] == 'false') {
+          res.redirect('accident_workrelated');
+        }
+      break;
+
+
+            
+        // Did you report the accident to the employer //
+        case 'report_got':
+        if(postData['report_employer'] == 'false') {
+          res.redirect('employer_details');
+        }
+      break;
+
+    }
+
+    next();
+
+  });
+
+  return router;
+}
